10cd424833beeb256f96c4afbec8d1cd6665b274
satoyuki <yuki01377@gmail.com>
2021-03-19 09:51:04 +0900

    front success

--- acm.tf
+++ acm.tf
@@ -13,24 +13,24 @@ resource "aws_acm_certificate" "acmCert" {
   }
 }
 
-# resource "aws_route53_record" "acmSite" {
-#   for_each = {
-#     for acm in aws_acm_certificate.acmCert.domain_validation_options: acm.domain_name => {
-#       name = acm.resource_record_name
-#       type = acm.resource_record_type
-#       record = acm.resource_record_value
-#     }
-#   }
-#   zone_id = aws_route53_zone.root.zone_id
-#   ttl = 60
-#   name = each.value.name
-#   type = each.value.type
-#   records = [each.value.record]
-# }
+resource "aws_route53_record" "acmSite" {
+  for_each = {
+    for acm in aws_acm_certificate.acmCert.domain_validation_options: acm.domain_name => {
+      name = acm.resource_record_name
+      type = acm.resource_record_type
+      record = acm.resource_record_value
+    }
+  }
+  allow_overwrite = true
+  zone_id = aws_route53_zone.root.zone_id
+  ttl = 60
+  name = each.value.name
+  type = each.value.type
+  records = [each.value.record]
+}
 
-# resource "aws_acm_certificate_validation" "acmCertValid" {
-#   certificate_arn = aws_acm_certificate.acmCert.arn
-#   validation_record_fqdns = [
-#     for record in aws_route53_record.acmSite: record.fqdn
-#   ]
-# }
+resource "aws_acm_certificate_validation" "acmCertValid" {
+  provider = aws.acm
+  certificate_arn = aws_acm_certificate.acmCert.arn
+  validation_record_fqdns = [for record in aws_route53_record.acmSite: record.fqdn]
+}
